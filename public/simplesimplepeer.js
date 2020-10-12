0cba9719c40b3c0cb3c5bdbdd38d43f904bde8a9
Shawn Van Every <Shawn.Van.Every@nyu.edu>
2020-10-12 17:07:02 -0400

    First

---
+++ public/simplesimplepeer.js
@@ -0,0 +1,228 @@
+/**
+ *
+ * @class SimpleSimplePeer
+ * @constructor
+ * @param {String} [something] blah blah blah.
+ * @example
+ *  		
+ *	function setup() {
+ *		 createCanvas(400, 300);
+ *       let video = createCapture();       
+*		 ssp = new SimpleSimplePeer(video.elem)
+*		 ssp.blahblah
+
+        // video.elem.srcObject
+*
+*		 let c = createCanvas(400, 300);
+*		 ssp = new SimpleSimplePeer(c.elem)
+*		 ssp.blahblah
+
+        // MediaStream = canvas.captureStream(frameRate);
+*	}
+*/
+class SimpleSimplePeer {
+
+
+    constructor(sketch, type, elem, room) {
+
+        this.CAPTURE = 1;
+        this.CANVAS = 2;
+
+        this.sketch = sketch;
+        this.simplepeers = [];
+        this.socket = io.connect();
+        
+        console.log(elem.elt);
+    
+        if (type == this.CANVAS) {
+            this.mystream = elem.elt.captureStream(30);
+        } else {
+            // Assume it is CAPTURE
+            this.mystream = elem;
+        }
+
+        this.socket.on('connect', () => {
+            console.log("Socket Connected");
+            console.log("My socket id: ", this.socket.id);
+
+            // Sends back a list of users in the room
+            this.socket.emit("room_connect", window.location.href);
+        });
+
+        this.socket.on('disconnect', (data) => {
+            console.log("Socket disconnected");
+        });
+
+        this.socket.on('peer_disconnect', (data) => {
+            console.log("simplepeer has disconnected " + data);
+            for (let i = 0; i < this.simplepeers.length; i++) {
+                if (this.simplepeers[i].socket_id == data) {
+                    console.log("Removing simplepeer: " + i);
+                    this.simplepeers.splice(i,1);
+                } 
+            }			
+        });			
+
+        // Receive listresults from server
+        this.socket.on('listresults', (data) => {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                // Make sure it's not us
+                if (data[i] != this.socket.id) {	
+
+                    // create a new simplepeer and we'll be the "initiator"			
+                    let simplepeer = new SimplePeerWrapper(this,
+                        true, data[i], this.socket, this.mystream
+                    );
+
+                    // Push into our array
+                    this.simplepeers.push(simplepeer);	
+                }
+            }
+        });
+            
+        this.socket.on('signal', (to, from, data) => {
+
+            console.log("Got a signal from the server: ", to, from, data);
+
+            // to should be us
+            if (to != this.socket.id) {
+                console.log("Socket IDs don't match");
+            }
+
+            // Look for the right simplepeer in our array
+            let found = false;
+            for (let i = 0; i < this.simplepeers.length; i++)
+            {
+                
+                if (this.simplepeers[i].socket_id == from) {
+                    console.log("Found right object");
+                    // Give that simplepeer the signal
+                    this.simplepeers[i].inputsignal(data);
+                    found = true;
+                    break;
+                }
+            
+            }	
+            if (!found) {
+                console.log("Never found right simplepeer object");
+                // Let's create it then, we won't be the "initiator"
+                let simplepeer = new SimplePeerWrapper(this,
+                    false, from, this.socket, this.mystream
+                );
+                
+                // Push into our array
+                this.simplepeers.push(simplepeer);	
+                    
+                // Tell the new simplepeer that signal
+                simplepeer.inputsignal(data);
+            }
+        });
+    }
+
+    on(event, callback) {
+        if (event == 'stream') {
+            this.onStream(callback);
+        }
+    }
+
+    onStream(callback) {
+        this.onStreamCallback = callback;
+    }
+
+
+
+    callOnStreamCallback(domElement) {
+        if (this.onStreamCallback) {
+
+            //////////////////////
+            // Copied from createCapture and addElement in p5.js source 10/12/2020
+            //const videoEl = addElement(domElement, this.sketch, true);
+            document.body.appendChild(domElement);
+            let videoEl = new p5.MediaElement(domElement, this.sketch);
+            this.sketch._elements.push(videoEl);
+
+            videoEl.loadedmetadata = false;
+            // set width and height onload metadata
+            domElement.addEventListener('loadedmetadata', function() {
+              domElement.play();
+              if (domElement.width) {
+                videoEl.width = domElement.width;
+                videoEl.height = domElement.height;
+              } else {
+                videoEl.width = videoEl.elt.width = domElement.videoWidth;
+                videoEl.height = videoEl.elt.height = domElement.videoHeight;
+              }
+              videoEl.loadedmetadata = true;
+            });
+            /////////////////////////////
+
+            this.onStreamCallback(videoEl);
+        }
+        else {
+            console.log("no onStreamCallback set");
+        }
+    }
+}
+
+// A wrapper for simplepeer as we need a bit more than it provides
+class SimplePeerWrapper {
+
+    constructor(ssp, initiator, socket_id, socket, stream) {
+        this.simplepeer = new SimplePeer({
+            initiator: initiator,
+            trickle: false
+        });				
+
+        this.supersimplepeer = ssp;
+
+        // Their socket id, our unique id for them
+        this.socket_id = socket_id;
+
+        // Socket.io Socket
+        this.socket = socket;
+
+        // Our video stream - need getters and setters for this
+        this.stream = stream;
+
+        // simplepeer generates signals which need to be sent across socket
+        this.simplepeer.on('signal', data => {						
+            this.socket.emit('signal', this.socket_id, this.socket.id, data);
+        });
+
+        // When we have a connection, send our stream
+        this.simplepeer.on('connect', () => {
+            console.log('CONNECT')
+            console.log(this.simplepeer);
+            //p.send('whatever' + Math.random())
+
+            // Let's give them our stream
+            this.simplepeer.addStream(stream);
+            console.log("Send our stream");
+        });
+
+        // Stream coming in to us
+        this.simplepeer.on('stream', stream => {
+            console.log('Incoming Stream');
+
+            // This should really be a callback
+            // Create a video object
+            let ovideo = document.createElement("VIDEO");
+            ovideo.id = this.socket_id;
+            ovideo.srcObject = stream;
+            ovideo.muted = false;
+            ovideo.onloadedmetadata = function(e) {
+                ovideo.play();
+            };					
+            //document.body.appendChild(ovideo);
+            console.log(ovideo);
+
+            this.supersimplepeer.callOnStreamCallback(ovideo);
+        });					
+    }
+
+    inputsignal(sig) {
+        this.simplepeer.signal(sig);
+    }
+
+}		
---
+++ server.js
@@ -0,0 +1,113 @@
+// We need the file system here
+var fs = require('fs');
+				
+// Express is a node module for building HTTP servers
+var express = require('express');
+var app = express();
+
+// Tell Express to look in the "public" folder for any files first
+app.use(express.static('public'));
+
+// If the user just goes to the "route" / then run this function
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+// Here is the actual HTTP server 
+// In this case, HTTPS (secure) server
+var https = require('https');
+
+// Security options - key and certificate
+var options = {
+  key: fs.readFileSync('star_itp_io.key'),
+  cert: fs.readFileSync('star_itp_io.pem')
+};
+
+// We pass in the Express object and the options object
+var httpServer = https.createServer(options, app);
+
+// Default HTTPS port
+httpServer.listen(443);
+
+/* 
+This server simply keeps track of the peers all in one big "room"
+and relays signal messages back and forth.
+*/
+let rooms = {};
+//let peers = [];
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(httpServer);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection', 
+
+	// We are given a websocket object in our function
+	function (socket) {
+
+		console.log("We have a new client: " + socket.id);
+		// peers.push({socket: socket});
+
+		socket.on('room_connect', function(room) {
+			console.log('room_connect', room);
+
+			if (!rooms.hasOwnProperty(room)) {
+				console.log("room doesn't exist, creating it");
+				rooms[room] = [];
+			}
+			rooms[room].push(socket);
+			socket.room = room;
+
+			console.log(rooms);
+
+			let ids = [];
+			for (let i = 0; i < rooms[socket.room].length; i++) {
+				ids.push(rooms[socket.room][i].id);
+			}
+			console.log("ids length: " + ids.length);
+			socket.emit('listresults', ids);
+		});
+
+		socket.on('list', function() {
+			let ids = [];
+			for (let i = 0; i < rooms[socket.room].length; i++) {
+				ids.push(rooms[socket.room][i].id);
+			}
+			console.log("ids length: " + ids.length);
+			socket.emit('listresults', ids);			
+		});
+		
+		// Relay signals back and forth
+		socket.on('signal', (to, from, data) => {
+			console.log("SIGNAL", to, data);
+			let found = false;
+			for (let i = 0; i < rooms[socket.room].length; i++) {
+				console.log(rooms[socket.room][i].id, to);
+				if (rooms[socket.room][i].id == to) {
+					console.log("Found Peer, sending signal");
+					rooms[socket.room][i].emit('signal', to, from, data);
+					found = true;
+					break;
+				}				
+			}	
+			if (!found) {
+				console.log("never found peer");
+			}
+		});
+				
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+			if (rooms[socket.room]) { // Check on this
+				for (let i = 0; i < rooms[socket.room].length; i++) {
+					if (rooms[socket.room][i].id == socket.id) {
+						rooms[socket.room].splice(i,1);
+					} else {
+						rooms[socket.room][i].emit('peer_disconnect', socket.id);
+					}
+				}			
+			}
+		});
+	}
+);
